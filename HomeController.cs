4103baf34758ee057f90978a4c71be105ac266b8
AstonXShubh <20027007@students.koi.edu.au>
2025-08-12 15:23:11 +1000

    Create HomeController.cs

---
+++ HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tutorial6ex2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This my name application shubham description page.";
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Our services page.";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
